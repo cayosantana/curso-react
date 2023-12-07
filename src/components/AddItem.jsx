38a2779b18ef8c9b74d0ff70ee58c03567256566
Cayo Santana <cc.santana.js@gmail.com>
2023-12-07 16:57:48 -0300

    Atualizei

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './AddItem.css'
+import Button from './Button';
 
 const AddItem = () => {
-    return ( 
-        <input type="text" />
-     );
+    return (
+        <div className='add-item-container'>
+            <input className='add-item-input' type="text" />
+            <div className='add-button-container'>
+                <Button>Adicionar</Button>
+            </div>
+        </div>
+    )
 }
  
 export default AddItem;
